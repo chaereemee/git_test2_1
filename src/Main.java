1313cc1c55391ebe90ff8c246d3be8b7ff95a2b1
Lee, Chaerim <leechaerim214@gmail.com>
2023-10-17 16:33:14 +0900

    Main.java 수정

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        Hello h = new Hello();
-        h.say();
+        Hello hello = new Hello();
+        hello.say();
     }
 
 }
